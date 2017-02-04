1104758c1cc5b0472c166679486b8ab4235d096d
zmm <1614180462@qq.com>
2017-02-04 16:40:55 +0800

    添加一些中间件

--- app/Http/Middleware/AdminLogin.php
+++ app/Http/Middleware/AdminLogin.php
@@ -3,22 +3,20 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
+class AdminLogin
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/');
+        if(!session('user')){
+            return redirect('admin/login');
         }
 
         return $next($request);
